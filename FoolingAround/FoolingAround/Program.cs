a7bf836d6e9fbeda7087dc08ad355fae1408fa73
Jackson Haenchen <jacksonhaenchen@gmail.com>
2014-10-07 19:13:28 -0500

    Initial Commit

---
+++ FoolingAround/FoolingAround/Program.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Microsoft.Win32;
+using Timer = System.Timers.Timer;
+
+namespace FoolingAround
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+                Console.WriteLine("Choose your option:\nFinbonacci\nTyping\nHangman");
+                var response = Console.ReadLine();
+                switch (response.ToLower())
+                {
+                    case ("fibonacci"):
+                        Console.Clear();
+                        fibonacci();
+                        break;
+                    case ("typing"):
+                        Console.Clear();
+                        typingGame();
+                        break;
+                    case ("hangman"):
+                        Console.Clear();
+                        Console.WriteLine("What's the thing to guess?");
+                        var toGuess = Console.ReadLine();
+                        hangMan(toGuess);
+                        break;
+                }
+            }
+            
+        }
+
+        private static void fibonacci()
+        {
+            int howManyIterations = int.Parse(Console.ReadLine());
+            var theArray = new double[howManyIterations + 2];
+            theArray[0] = 0;
+            theArray[1] = 1;
+            int counter = 2;
+            for (counter = 2; counter < howManyIterations; counter++)
+            {
+                theArray[counter] = theArray[counter - 1] + theArray[counter - 2];
+                Console.WriteLine("{0} + {1} = {2}", theArray[counter - 2], theArray[counter - 1],
+                    theArray[counter - 1] + theArray[counter - 2]);
+            }
+        }
+        
+        private static void flowGame()
+        {
+        }
+
+        private static void namePlay()
+        {
+            Console.Clear();
+            Console.WriteLine("Loading dictionary, please wait...");
+            string contents =
+                new WebClient().DownloadString(@"http://www.mieliestronk.com/corncob_lowercase.txt");
+            Console.Clear();
+            string[] dictionary = contents.Split(Environment.NewLine.ToCharArray());
+            dictionary = dictionary.Where((value, index) => index % 2 == 0).ToArray();
+            Array.Sort(dictionary, compareStringsByLength);
+            var dictionaryList = dictionary.ToList();
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            var newName = name;
+            int counter = 0;
+            List<KeyValuePair<string, string>> matches = new List<KeyValuePair<string, string>>();
+            
+            for (int i = 0; i < 26; i++)
+            {
+                newName = incrementString(newName);
+                foreach (var term in dictionaryList)
+                {
+                    if (term.Contains(newName))
+                    {
+                        try
+                        {
+                            matches.Add(new KeyValuePair<string, string>(newName,term));
+                        }
+                        catch (System.ArgumentException)
+                        {
+                            
+                            
+                        }
+                        
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine("Found derivations: " + counter+"\n");
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match.Key+" : "+match.Value+"\n");
+            }
+            Console.ReadLine();
+        }
+
+        private static string incrementString(string t)
+        {
+            var array = t.ToCharArray();
+            for (int i = 0; i < t.Length; i++)
+            {
+                array[i]++;
+
+            }
+            return new string(array);
+        }
+
+        private static void hangMan(string title)
+        {
+            string titleBackup = title;
+            var theGuess = string.Empty;
+            bool justOne = false;
+            for (int i = 0; i < title.Length; i++)
+            {
+                theGuess += " _ ";
+            }
+            
+            while (true)
+            {
+                if (justOne)
+                {
+                    theGuess = string.Empty;
+                    for (int i = 0; i < title.Length; i++)
+                    {
+                        theGuess += " _ ";
+                    }
+                    justOne = false;
+                }
+                Console.Clear();
+                hangmanDisplayObj.display(hangmanDisplayObj.count);
+                Console.WriteLine(theGuess);
+                Console.WriteLine("Guess a letter...");
+                var letterLine = Console.ReadLine();
+                var letter = letterLine[0];
+                bool goodGuess = false;
+                for (int i = 0; i < title.Length; i++)
+                {
+                    if (title[i].ToString().ToLower().ToCharArray()[0] == letter.ToString().ToLower().ToCharArray()[0])
+                    {
+                        goodGuess = true;
+                        int underscoreCounter = -1;
+                        for (int k = 0; k < theGuess.Length; k++)
+                        {
+                            if (theGuess[k] == "_".ToCharArray()[0])
+                            {
+                                underscoreCounter++;
+                            }
+                            else if (theGuess[k] != " ".ToCharArray()[0])
+                            {
+                                underscoreCounter++;
+                            }
+                            if (underscoreCounter == i)
+                            {
+                                theGuess = theGuess.Remove(k, 1).Insert(k, letter.ToString());
+                                break;
+                            }
+                        }
+
+
+                    }
+                    
+                }
+                if (!goodGuess)
+                {
+                    hangmanDisplayObj.display();
+                    if (hangmanDisplayObj.count == 7)
+                    {
+                        Console.Clear();
+                        hangmanDisplayObj.display(hangmanDisplayObj.count);
+                        Console.WriteLine("Game over man!!!");
+                        Console.WriteLine("The answer was: "+title);
+                        Console.WriteLine("Enter any key to continue...");
+                        Console.ReadLine();
+                        hangmanDisplayObj.reset();
+                        justOne = true;
+                        
+                    }
+                }
+                if (theGuess.Replace(" ","").ToLower().Replace("_","") == title.ToLower().Replace(" ",""))
+                {
+                    Console.Clear();
+                    Console.WriteLine("You got it! The answer was "+title);
+                    Console.WriteLine("Type anything to play again...");
+                    Console.ReadLine();
+                    hangmanDisplayObj.reset();
+                    justOne = true;
+                }
+            }
+        }
+
+        private static class hangmanDisplayObj
+        {
+            private static string[] states = new string[8];
+            public static int count = 0;
+            static hangmanDisplayObj()
+            {
+                states[0] = "\n";
+                states[1] = "  O \n";
+                states[2] = "  O  \n  |\n";
+                states[3] = "  O  \n  |/\n";
+                states[4] = "  O  \n \\|/\n";
+                states[5] = "  O  \n \\|/  \n  | \n";
+                states[6] = "  O  \n \\|/  \n  | \n /\n";
+                states[7] = "  O  \n \\|/  \n  | \n / \\\n";
+            }
+
+            public static void display()
+            {
+                Console.Write(states[count]+"\n \n \n \n \n");
+                count++;
+            }
+
+            public static void display(int newCount)
+            {
+                Console.Write(states[newCount]);
+                count = newCount;
+            }
+            public static void reset()
+            {
+                count = 0;
+            }
+        }
+
+        private static int compareStringsByLength(string x, string y)
+        {
+            if (x.Length < y.Length)
+            {
+                return -1;
+            }
+            if (x.Length > y.Length)
+            {
+                return 1;
+            }
+            if (x.Length == y.Length)
+            {
+                return 0;
+            }
+            return 0;
+        }
+
+        private static void typingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Loading dictionary, please wait...");
+            string contents =
+                new WebClient().DownloadString(@"http://www.mieliestronk.com/corncob_lowercase.txt");
+            Console.Clear();
+            string[] dictionary = contents.Split(Environment.NewLine.ToCharArray());
+            dictionary = dictionary.Where((value, index) => index%2 == 0).ToArray();
+            Array.Sort(dictionary,compareStringsByLength);
+            var timer = new Timer();
+            var stopwatch = new Stopwatch();
+            var score = 0;
+            int[] range = new int[2];
+            range[0] = 0;
+            range[1] = 1000;
+            int rangeIncrementer = 1000;
+            float secondToWait = 5000;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(string.Format( "Score: {0}",score));
+                stopwatch.Reset();
+                stopwatch.Start();
+                var randomObject = new Random();
+                int num = randomObject.Next(range[0],range[1]);
+                num = num%dictionary.Length;
+                Console.WriteLine(dictionary[num]);
+                string response = string.Empty;
+
+
+                try
+                {
+                    response = Reader.ReadLine(int.Parse(secondToWait.ToString()));
+                }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("\nSorry, you waited too long.");
+                    score--;
+                    response = "var:NoResponse";
+                    Thread.Sleep(1000);
+                }
+                if (response.ToLower() == dictionary[num].ToLower())
+                {
+                    Console.WriteLine("Got it!");
+                    score++;
+                    secondToWait -= 100;
+                    range[0] += rangeIncrementer;
+                    range[1] += rangeIncrementer;
+                    rangeIncrementer *= 2;
+                    Thread.Sleep(2000);
+                }
+                else if (response.ToLower() != dictionary[num].ToLower() && response != "var:NoResponse")
+                {
+                    Console.WriteLine("\nNope!");
+                    score--;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ MicAndNotes/MicAndNotes/MainWindow.xaml.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicAndNotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            bw.WorkerSupportsCancellation = false;
+            bw.WorkerReportsProgress = false;
+            bw.DoWork +=
+    new DoWorkEventHandler(bw_DoWork);
+        }
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private List<TimeNote> timesForNote = new List<TimeNote>();
+
+        private class TimeNote
+        {
+            public TimeSpan occurance = new TimeSpan();
+            public string note;
+
+            public TimeNote(TimeSpan o, string n)
+            {
+                occurance = o;
+                note = n;
+            }
+
+            public string serialize()
+            {
+                return occurance.ToString() + "," + note+",";
+            }
+        }
+
+        private int currentLine = 0;
+        private string lastTextFilled = "";
+        private bool wasLastKeyEnter = false;
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && wasLastKeyEnter)
+            {
+                
+                string toStore = Textbox.Text;
+                if (lastTextFilled != "")
+                {
+                    toStore = Textbox.Text.Replace(lastTextFilled, String.Empty);
+                }
+                lastTextFilled = Textbox.Text;
+                
+                
+                timesForNote.Add(new TimeNote(recordingTimer.Elapsed, toStore));
+                wasLastKeyEnter = false;
+                currentLine++;
+            }
+            else if (e.Key == Key.Enter)
+            {
+                wasLastKeyEnter = true;
+            }
+            else
+            {
+                wasLastKeyEnter = false;
+            }
+        }
+        Stopwatch recordingTimer = new Stopwatch();
+        private void RecordButton_Click(object sender, RoutedEventArgs e)
+        {
+            mciSendString("open new Type waveaudio Alias recsound", "", 0, 0);
+            recordingTimer.Start();
+            mciSendString("record recsound", "", 0, 0);
+            
+
+            
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            MediaPlayer play = new MediaPlayer();
+
+            play.Open(new Uri("c:\\result.wav"));
+            play.Position = (TimeSpan) e.Argument;
+            play.Play();
+            
+        }
+
+        BackgroundWorker bw = new BackgroundWorker();
+        private void PlayBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textArchive = Textbox.Text;
+            TimeSpan toStart = new TimeSpan(0);
+            
+            
+            
+            bw.RunWorkerAsync(toStart);
+            
+
+
+            
+            //Textbox.Text = "";
+            
+            //for (int i = 0; i < timesForNote.Count; i++)
+            //{
+               
+            //    TheLabel.Content = i.ToString();
+            //    while (t.Elapsed < timesForNote[i])
+            //    {
+            //        Thread.Sleep(1);
+            //    }
+            //}
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                bool shouldContinue = true;
+                int result = 0;
+                int counter = 0;
+                Stopwatch t = new Stopwatch();
+                
+                t.Start();
+
+                while(shouldContinue)
+                {
+                    result++;
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        try
+                        {
+                            if (t.Elapsed+toStart < timesForNote[counter].occurance) Textbox.Text = timesForNote[counter].note;
+                            else
+                            {
+
+                                counter++;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            
+                            shouldContinue = false;
+                            Textbox.Text = textArchive;
+                            Textbox.ScrollToEnd();
+                        }
+                    
+                        
+                    }));
+                    Thread.Sleep(1);
+                }
+            });
+            
+
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            recordingTimer.Stop();
+            mciSendString("save recsound c:\\result.wav", "", 0, 0);
+            mciSendString("close recsound ", "", 0, 0);
+            theSlider.Maximum = recordingTimer.Elapsed.Ticks;
+        }
+
+        
+
+        private void FromCursor_Click(object sender, RoutedEventArgs e)
+        {
+            string textArchive = Textbox.Text;
+            TimeSpan toStart = new TimeSpan();
+            int toPlayIndex = Textbox.CaretIndex;
+            int lineIndex = Textbox.GetLineIndexFromCharacterIndex(toPlayIndex);
+            String text = Textbox.GetLineText(lineIndex);
+            for (int i = 0; i < timesForNote.Count; i++)
+            {
+                if (timesForNote[i].note.Contains(text))
+                {
+                    if (i > 0)
+                    {
+                        toStart = timesForNote[i - 1].occurance;
+                    }
+                    else
+                    {
+                        toStart = new TimeSpan(0);
+                    }
+
+                }
+            }
+
+            
+
+            bw.RunWorkerAsync(toStart);
+
+
+
+
+            //Textbox.Text = "";
+
+            //for (int i = 0; i < timesForNote.Count; i++)
+            //{
+
+            //    TheLabel.Content = i.ToString();
+            //    while (t.Elapsed < timesForNote[i])
+            //    {
+            //        Thread.Sleep(1);
+            //    }
+            //}
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                bool shouldContinue = true;
+                int result = 0;
+                int counter = 0;
+                Stopwatch t = new Stopwatch();
+
+                t.Start();
+
+                while (shouldContinue)
+                {
+                    result++;
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        try
+                        {
+                            if (t.Elapsed + toStart < timesForNote[counter].occurance) Textbox.Text = timesForNote[counter].note;
+                            else
+                            {
+                                counter++;
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                            shouldContinue = false;
+                            Textbox.Text = textArchive;
+                            Textbox.ScrollToEnd();
+                        }
+
+
+                    }));
+                    Thread.Sleep(1);
+                }
+            });
+        }
+
+        
+
+        private void theSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var sliderValue = new TimeSpan((long) theSlider.Value);
+            for (int i = 0; i < timesForNote.Count-1; i++)
+            {
+                int theFirst = timesForNote[i].occurance.CompareTo(sliderValue);
+                int theSecond = timesForNote[i + 1].occurance.CompareTo(sliderValue);
+                if (theFirst == -1 && theSecond == 1)
+                {
+                    Textbox.Text = timesForNote[i + 1].note;
+                }
+                
+                else if (i == 0 && timesForNote[i].occurance.CompareTo(sliderValue) == 1)
+                {
+                    Textbox.Text = timesForNote[i].note;
+                }
+                else if (i == timesForNote.Count - 2 && timesForNote[i + 1].occurance.CompareTo(sliderValue) == -1)
+                {
+                    Textbox.Text = timesForNote[i + 1].note;
+                }
+            }
+        }
+
+        private void SaveToFile()
+        {
+           
+            //File.WriteAllLines("c:\\toSave.txt",timesForNote);
+        }
+        
+    }
+}
